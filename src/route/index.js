01b5ad781cf766159d66d0126e94f64390a010a3
CLII412 <sw412fr@gmail.com>
2023-03-13 21:13:16 +0200

    init

--- src/route/index.js
+++ src/route/index.js
@@ -7,99 +7,158 @@ const router = express.Router()
 
 // router.get Створює нам один ентпоїнт
 
-router.get('/car', function (req, res) {
+router.get('/facebook', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('car', {
-    make: 'Toyota',
-    model: 'Camry',
-    year: 2022,
-    color: 'silver',
-    features: {
-      interior: {
-        seats: {
-          material: 'leather',
-          color: 'black',
-          heated: true,
-          ventilated: true,
-        },
-        dashboard: {
-          material: 'plastic',
-          color: 'black',
-          display: {
-            type: 'LCD',
-            size: 10.1,
-            resolution: '1280x720',
-            touchscreen: true,
+  res.render('facebook', {
+    name: 'Facebook',
+    users: [
+      {
+        id: 1,
+        name: 'John Doe',
+        gender: 'Male',
+        age: 30,
+        friends: [
+          {
+            id: 2,
+            name: 'Jane Smith',
+            gender: 'Female',
+            age: 27,
+            mutual_friends: [
+              {
+                id: 4,
+                name: 'Samantha Lee',
+                gender: 'Female',
+                age: 25,
+              },
+            ],
           },
-        },
-        audio: {
-          system: 'JBL',
-          speakers: 8,
-          subwoofer: true,
-          bluetooth: true,
-          USB: true,
-        },
+          {
+            id: 5,
+            name: 'Mike Williams',
+            gender: 'Male',
+            age: 28,
+            mutual_friends: [
+              {
+                id: 7,
+                name: 'David Kim',
+                gender: 'Male',
+                age: 29,
+              },
+            ],
+          },
+        ],
+        groups: [
+          {
+            id: 1,
+            name: 'Hiking Enthusiasts',
+            description:
+              'A group for people who love hiking and the great outdoors',
+            members: [
+              {
+                id: 8,
+                name: 'Sarah Johnson',
+                gender: 'Female',
+                age: 25,
+              },
+            ],
+          },
+          {
+            id: 2,
+            name: 'Foodies United',
+            description:
+              'A group for food lovers to share recipes and restaurant recommendations',
+            members: [
+              {
+                id: 5,
+                name: 'Mike Williams',
+                gender: 'Male',
+                age: 28,
+              },
+              {
+                id: 8,
+                name: 'Sarah Johnson',
+                gender: 'Female',
+                age: 25,
+              },
+            ],
+          },
+        ],
       },
-      exterior: {
-        wheels: {
-          size: 18,
-          type: 'alloy',
-          color: 'silver',
-        },
-        headlights: {
-          type: 'LED',
-          brightness: 'high',
-          automatic: true,
-        },
-        sunroof: {
-          type: 'panoramic',
-          size: 'large',
-          automatic: true,
-        },
+      {
+        id: 16,
+        name: 'Amy Lee',
+        gender: 'Female',
+        age: 30,
+        friends: [
+          {
+            id: 15,
+            name: 'Peter Kim',
+            gender: 'Male',
+            age: 32,
+            mutual_friends: [
+              {
+                id: 17,
+                name: 'David Chen',
+                gender: 'Male',
+                age: 33,
+              },
+              {
+                id: 19,
+                name: 'Erica Wong',
+                gender: 'Female',
+                age: 29,
+                mutual_friends: [
+                  {
+                    id: 20,
+                    name: 'Alex Lee',
+                    gender: 'Male',
+                    age: 31,
+                  },
+                ],
+              },
+            ],
+            groups: [
+              {
+                id: 3,
+                name: 'Travel Addicts',
+                description:
+                  'A group for people who love to travel and explore new places',
+                members: [
+                  {
+                    id: 22,
+                    name: 'Jackie Chen',
+                    gender: 'Female',
+                    age: 26,
+                  },
+                ],
+              },
+              {
+                id: 4,
+                name: 'Pet Lovers',
+                description:
+                  'A group for people who love their furry friends',
+                members: [
+                  {
+                    id: 16,
+                    name: 'Amy Lee',
+                    gender: 'Female',
+                    age: 30,
+                  },
+                  {
+                    id: 25,
+                    name: 'John Smith',
+                    gender: 'Male',
+                    age: 33,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       },
-      safety: {
-        airbags: {
-          front: 2,
-          side: 2,
-          knee: 2,
-          rear: 2,
-        },
-        assistance: {
-          blind_spot_monitoring: true,
-          rear_cross_traffic_alert: true,
-          lane_departure_warning: true,
-          adaptive_cruise_control: true,
-          collision_warning: true,
-        },
-      },
-    },
-    engine: {
-      type: 'gasoline',
-      displacement: 2.5,
-      horsepower: 206,
-      torque: 186,
-      transmission: {
-        type: 'automatic',
-        gears: 8,
-      },
-    },
-    fuel_economy: {
-      city: 28,
-      highway: 39,
-      combined: 32,
-    },
-    price: {
-      base: 25900,
-      destination: 995,
-      options: {
-        navigation: 1200,
-        moonroof: 800,
-        premium_paint: 595,
-      },
-      total: 28990,
-    },
+    ],
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
