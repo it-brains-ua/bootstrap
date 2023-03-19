56858324c3314bf718458e6852d2b831c278aa83
Turaiev Kyrylo <38906421+2rv@users.noreply.github.com>
2023-03-19 19:16:21 +0200

    upd

--- src/route/index.js
+++ src/route/index.js
@@ -8,11 +8,11 @@ const router = express.Router()
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/shopproduct', function (req, res) {
+router.get('/shopnews', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('shopproduct', {
+  res.render('shopnews', {
     layout: 'shop',
     navigation: {
       links: [
@@ -34,60 +34,45 @@ router.get('/shopproduct', function (req, res) {
 
     breadcrumb: [
       { name: 'Home', url: 'https://github.com/' },
-      { name: 'PC', url: 'https://www.google.com/' },
-      { name: 'Windows', url: 'https://github.com/' },
-      { name: 'Product PC #3123', url: null },
+      { name: 'News', url: 'https://www.google.com/' },
+      { name: 'Today', url: 'https://github.com/' },
+      { name: 'About cats', url: null },
     ],
 
-    productData: {
-      img: 'https://picsum.photos/400/200',
+    newsData: {
+      photo: 'https://picsum.photos/400/200',
 
-      info: {
-        title: "Product PC Asus 331 BC 671",
-        about: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        price: '1000$',
-        
-        category: [
-          { name: "HOT", isHot: true },
-          { name: "New", isNew: true },
-          { name: "Bonus", isBonus: true }
-        ]
+      head: {
+        heading: "About cats",
+        description: "Cats are adorable and beloved pets that have been domesticated for thousands of years. They are known for their independent and playful nature, as well as their agility and hunting skills. With their soft fur, purring, and cuddling, cats make great companions and can bring joy and relaxation to their owners. From domestic short-hairs to exotic breeds, cats come in many shapes, sizes, and colors, each with their own unique personalities. Whether they are napping in a sunny spot or chasing a toy, cats always bring a smile to their owner's faces.",
       },
 
-      actionList: [
-        { name: 'Buy', isWarning: true },
-        { name: 'Add to cart', isError: true },
-        { name: 'Like', isSuccess: true },
-        { name: 'Share', isGood: true },
-      ],
-
-      alert: {
-        isError: true,
-        text: "Danger! This is a danger alert — check it out!"
-      },
-
-      param: {
-        title: 'Product params',
-        list: [
-          'Экран 15.6',
-          'IPS (1920x1080)',
-          'Full HD'
-        ]
-      },
-
-      item: {
-        head: ["#", "color", "price"],
-
-        body: [
-          ["id312", 'red', '1100$'],
-          ["id532", 'blue', '1050$'],
-          ["id643", 'dark', '999$'],
-        ] 
-      }
+      body: [
+        {
+          title: "Cats have a keen sense of hearing",
+          text: "With their highly sensitive ears, cats can detect sounds that are too faint for humans to hear, including the ultrasonic calls of rodents and other small prey."
+        },
+        {
+          title: "Cats are natural hunters",
+          text: "Thanks to their sharp claws, quick reflexes, and stealthy movements, cats are excellent hunters. They can catch mice, rats, birds, and other prey with ease."
+        },
+        {
+          title: "Cats are independent creatures",
+          text: "Cats are known for their independent nature, and unlike dogs, they don't require constant attention and affection from their owners. They are perfectly content to entertain themselves with toys and explore their surroundings on their own."
+        },
+        {
+          title: "Cats have a unique way of communicating",
+          text: "From meows and purrs to chirps and trills, cats use a variety of sounds to communicate with their owners and other cats. They also use body language, such as tail movements and ear positions, to convey their moods and intentions."
+        },
+        {
+          title: "Cats are good for your health",
+          text: "Studies have shown that owning a cat can have numerous health benefits, including lower stress levels, reduced risk of heart disease and stroke, and improved mental health. Plus, cats make great companions and can provide a source of comfort and emotional support."
+        }
+      ]
     },
 
-    goodsOtherBlock: {
-      title: 'See other products',
+    newsOtherBlock: {
+      title: 'See other news',
       cards: [
         {
           title: 'COVID-19 Vaccinations Begin in Europe',
