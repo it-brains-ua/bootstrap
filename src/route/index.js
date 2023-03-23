7d11eacdb1b964c3efedc9b3e9dc963769ff3514
CLII412 <sw412fr@gmail.com>
2023-03-23 14:50:24 +0200

    upd

--- src/route/index.js
+++ src/route/index.js
@@ -40,14 +40,14 @@ router.get('/shophome', function (req, res) {
           description:
             'As the world continues to grapple with the ongoing COVID-19 pandemic, Europe begins its vaccination rollout, with healthcare workers and the elderly among the first to receive the vaccine.',
           isTop: false,
-          idNew: false,
+          isNew: false,
         },
         {
           title: 'Tesla Stock Surges After Record Sales',
           description:
             "Tesla's stock price jumps 10% following the company's announcement of record sales in the last quarter, thanks in part to strong demand for its electric vehicles.",
           isTop: true,
-          idNew: true,
+          isNew: true,
         },
         {
           title:
@@ -55,14 +55,14 @@ router.get('/shophome', function (req, res) {
           description:
             'A new study published in the Journal of the American Medical Association finds that practicing mindfulness meditation can lead to improved mental health outcomes for people struggling with depression and anxiety.',
           isTop: false,
-          idNew: false,
+          isNew: false,
         },
         {
           title: 'NASA Launches New Mars Rover',
           description:
             "NASA's Perseverance rover successfully launches on a mission to explore Mars, with the goal of collecting samples of the planet's surface and searching for signs of ancient microbial life.",
           isTop: false,
-          idNew: true,
+          isNew: true,
         },
         {
           title:
@@ -70,7 +70,7 @@ router.get('/shophome', function (req, res) {
           description:
             'Shares of GameStop surge as amateur traders on the social media platform Reddit rally to drive up the price, in what some are calling a battle between Wall Street and Main Street.',
           isTop: false,
-          idNew: false,
+          isNew: false,
         },
         {
           title:
@@ -78,7 +78,7 @@ router.get('/shophome', function (req, res) {
           description:
             'In an effort to combat climate change, the UK government announces a plan to ban the sale of new gas-powered cars and vans by 2030, with hybrid vehicles to follow in 2035.',
           isTop: true,
-          idNew: false,
+          isNew: false,
         },
         {
           title:
@@ -86,14 +86,14 @@ router.get('/shophome', function (req, res) {
           description:
             'A new study published in the Journal of the American Medical Association suggests that regular exercise can help people live longer, with participants who exercised regularly having a lower risk of premature death.',
           isTop: false,
-          idNew: false,
+          isNew: false,
         },
         {
           title: 'Amazon Expands Grocery Delivery Service',
           description:
             'Amazon announces an expansion of its grocery delivery service, with plans to offer free delivery to Prime members on orders over $35 and to expand its selection of fresh and organic produce.',
           isTop: false,
-          idNew: true,
+          isNew: true,
         },
       ],
     },
@@ -128,7 +128,7 @@ router.get('/shophome', function (req, res) {
           description:
             'The latest iPhone model features a new A15 Bionic chip, improved camera system, and longer battery life.',
           isHot: false,
-          idNew: false,
+          isNew: false,
         },
         {
           image: 'https://picsum.photos/400/200',
@@ -136,7 +136,7 @@ router.get('/shophome', function (req, res) {
           description:
             "Apple's high-end laptop features a 16-inch Retina display, powerful M1 Pro or M1 Max chip, and up to 64GB of RAM.",
           isHot: true,
-          idNew: false,
+          isNew: false,
         },
         {
           image: 'https://picsum.photos/400/200',
@@ -144,7 +144,7 @@ router.get('/shophome', function (req, res) {
           description:
             "Apple's premium wireless earbuds feature active noise cancellation, a customizable fit, and up to 4.5 hours of listening time.",
           isHot: false,
-          idNew: false,
+          isNew: false,
         },
         {
           image: 'https://picsum.photos/400/200',
@@ -152,7 +152,7 @@ router.get('/shophome', function (req, res) {
           description:
             'This OLED TV boasts a 4K resolution, HDR support, and a high refresh rate for smooth motion handling.',
           isHot: false,
-          idNew: true,
+          isNew: true,
         },
         {
           image: 'https://picsum.photos/400/200',
@@ -160,7 +160,7 @@ router.get('/shophome', function (req, res) {
           description:
             'The latest iteration of the popular gaming console features a larger OLED screen and improved audio quality.',
           isHot: false,
-          idNew: false,
+          isNew: false,
         },
         {
           image: 'https://picsum.photos/400/200',
@@ -168,7 +168,7 @@ router.get('/shophome', function (req, res) {
           description:
             'This portable Bluetooth speaker features 360-degree sound, water-resistant construction, and up to 16 hours of battery life.',
           isHot: true,
-          idNew: true,
+          isNew: true,
         },
       ],
     },
